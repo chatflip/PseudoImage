fe02e9344bc95bde117dbc73acf4fb64ed344df5
chatflip <oo.chat.flip@gmail.com>
2024-11-21 03:41:59 +0900

    fix pydocstyle

--- src/main.py
+++ src/main.py
@@ -2,22 +2,16 @@ import argparse
 import logging
 
 import cv2
-from PseudoImage import PseudoImage
+
+from .PseudoImage import PseudoImage
 
 
-def main(args: argparse.Namespace) -> None:
-    log_level = getattr(logging, args.log_level)
-    logging.basicConfig(level=log_level)
-    pseudo_image_maker = PseudoImage(
-        args.image_scale,
-        args.font,
-        args.font_scale,
-    )
-    pseudo_image = pseudo_image_maker(args.src_path)
-    cv2.imwrite(args.dst_path, pseudo_image)
+def config() -> argparse.Namespace:
+    """Set the arguments for the main function.
 
-
-if __name__ == "__main__":
+    Returns:
+        argparse.Namespace: The arguments for the main function.
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument("--src_path", default="images/sample.jpg", type=str)
     parser.add_argument("--dst_path", default="images/image_pseudo.png", type=str)
@@ -31,4 +25,18 @@ if __name__ == "__main__":
     parser.add_argument("--font", default="FONT_HERSHEY_SIMPLEX", type=str)
     parser.add_argument("--font_scale", default=0.4, type=float)
     args = parser.parse_args()
-    main(args)
+    return args
+
+
+if __name__ == "__main__":
+    args = config()
+
+    log_level = getattr(logging, args.log_level)
+    logging.basicConfig(level=log_level)
+    pseudo_image_maker = PseudoImage(
+        args.image_scale,
+        args.font,
+        args.font_scale,
+    )
+    pseudo_image = pseudo_image_maker(args.src_path)
+    cv2.imwrite(args.dst_path, pseudo_image)
